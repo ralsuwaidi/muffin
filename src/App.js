4c0712e7f4e920a4d7d8e84615ca718375ff4d9e
rashed <x.suwaidi@gmail.com>
2022-08-13 13:00:41 +0400

    added modal image

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from 'react'
+import DetailModal from "./components/DetailModal";
 
-function App() {
+
+export default function App(){
+
+  const [showModal, setShowModal] = React.useState(false);
+
+  const handleClose = () => {
+    setShowModal(false);
+   };
+
+   const handleOpen = () => {
+    setShowModal(true);
+   };
+
   return (
     <div className="App container mx-auto ">
-
       <img className="App-background" />
 
 
       <div className="absolute inset-x-6 top-10">
-        <p className="font-bold text-3xl">
-        Muffin 
-        Discovery</p>
-        <p className="text-slate-400 text-sm">Sometimes the world should decide what you should eat</p>
+        <p className="font-bold text-3xl">Muffin Discovery</p>
+        <p className="text-slate-400 text-sm">
+          Sometimes the world should decide what you should eat
+        </p>
       </div>
-
-     
 
       <div className=" grid grid-cols-3 gap-1 mx-3 absolute inset-x-0 bottom-5">
-        <SelectionCard text="Breakfast" image="https://img.icons8.com/cotton/2x/sunny-side-up-eggs--v1.png"/>
-        <SelectionCard text="Lunch" image="https://img.icons8.com/cotton/2x/cheeseburger.png" />
-        <SelectionCard  text="Dessert" image="https://img.icons8.com/cotton/2x/piece-of-lemon-cake.png"/>
+        <SelectionCard
+          text="Breakfast"
+          onClick={() => setShowModal(true)}
+          image="https://img.icons8.com/cotton/2x/sunny-side-up-eggs--v1.png"
+        />
+        <SelectionCard
+          text="Lunch"
+          onClick={() => setShowModal(true)}
+          image="https://img.icons8.com/cotton/2x/cheeseburger.png"
+        />
+        <SelectionCard
+          text="Dessert"
+          onClick={() => setShowModal(true)}
+          image="https://img.icons8.com/cotton/2x/piece-of-lemon-cake.png"
+        />
       </div>
+
+      {showModal ? (
+        <DetailModal handleClose={handleClose}/>
+      ) : null}
 
     </div>
   );
 }
 
-export default App;
+
 
 function SelectionCard(props){
   return (
     <div className="mx-2">
-        <a href="#" className="block text-center py-2 max-w-sm bg-transparent rounded-lg border border-gray-100 hover:bg-gray-100">
+        <a href="#" onClick={props.onClick} className="block text-center py-2 max-w-sm bg-transparent rounded-lg border border-gray-100 hover:bg-gray-100">
         <img src={props.image} className="w-12 mx-auto"></img>
         <p className="mb-1 mt-2 text-xs font-bold tracking-tight dark:text-white">
           {props.text}
---
+++ src/components/DetailModal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./DetailModal.css"
+
+export default function DetailModal(props){
+
+return (
+    <>
+    <div
+      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+    >
+      <div className="relative w-auto">
+        {/*content*/}
+        <div className="border-0 w-screen DetailModal-height shadow-lg relative flex flex-col  bg-white outline-none focus:outline-none">
+          {/*header*/}
+         <div className="p-2">
+          {/*image and content*/}
+          <img className="DetailModal-img rounded" src="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80"/>
+         </div>
+          {/*footer*/}
+          <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+            <button
+              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={props.handleClose}
+            >
+              Close
+            </button>
+            <button
+              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={props.handleClose}
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </>
+)
+}
